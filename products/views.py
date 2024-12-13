a09057e79a669f316d61580149dd974e20321d04
Towbie Abiodun-Idowu <towbieidowu@gmail.com>
2024-12-13 05:10:38 +0000

    Create products template

--- products/views.py
+++ products/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from .models import Product
 
-# Create your views here.
+
+def mains(request):
+    """ A view to show all mains, including sorting """
+
+    # Filter products by category 'mains'
+    products = Product.objects.filter(category=1)
+
+    context = {
+        'products': products,
+    }
+
+    return render(request, 'products/products.html', context)
+
