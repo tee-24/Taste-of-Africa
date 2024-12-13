3b6e28ed17b63cd981b32b03d69d05cd2bd36b08
Towbie Abiodun-Idowu <towbieidowu@gmail.com>
2024-12-13 05:15:20 +0000

    Create views and urls for drinks and sides

--- products/urls.py
+++ products/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('mains/', views.mains, name='mains'),
+    path('sides/', views.sides, name='sides'),
+    path('drinks/', views.drinks, name='drinks')
 ]
--- products/views.py
+++ products/views.py
@@ -14,3 +14,33 @@ def mains(request):
 
     return render(request, 'products/products.html', context)
 
+def sides(request):
+    """ A view to show all sides, including sorting """
+
+
+    # Filter products by category 'sides'
+    products = Product.objects.filter(category=2)
+
+
+    context = {
+        'products': products,
+    }
+
+
+    return render(request, 'products/products.html', context)
+
+
+def drinks(request):
+    """ A view to show all drinks, including sorting """
+
+
+    # Filter products by category 'drinks'
+    products = Product.objects.filter(category=3)
+
+
+    context = {
+        'products': products,
+    }
+
+
+    return render(request, 'products/products.html', context)
