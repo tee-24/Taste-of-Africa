55220b6db6106d3230f943f63f15387fa68a5ff3
Towbie Abiodun-Idowu <towbieidowu@gmail.com>
2024-12-13 10:32:55 +0000

    Add bad context and delivery logic

--- taste_of_africa/settings.py
+++ taste_of_africa/settings.py
@@ -73,6 +73,7 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
                 'django.template.context_processors.media',
+                'bag.contexts.bag_contents'
             ],
         },
     },
@@ -151,3 +152,6 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+FREE_DELIVERY_THRESHOLD = 50
+STANDARD_DELIVERY_PERCENTAGE = 10
