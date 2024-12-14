b3fb07fa2f7836cbc0d78da83ada5b63d7bca294
Towbie Abiodun-Idowu <towbieidowu@gmail.com>
2024-12-14 10:39:59 +0000

    Add Stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51Q6D2EHctCDJgekX6S9y4iAMBakq8eQDkPAhrmntZdY36kRNBh5ADQobaCgvvS2piO1c78LL3qY1E2o78Fb45PUe00vNJ1IbDC',
+        'client_secret': 'test client secret'
     }
 
     return render(request, template, context)
