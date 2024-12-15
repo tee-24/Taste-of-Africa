ff227bcf983ca8e52aa7ce903e259b8c6c5e3f60
Towbie Abiodun-Idowu <towbieidowu@gmail.com>
2024-12-15 11:58:54 +0000

    Add edit product functionality

--- products/urls.py
+++ products/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('sides/', views.sides, name='sides'),
     path('drinks/', views.drinks, name='drinks'),
     path('<int:product_id>/', views.product_detail, name='product_detail'),
-    path('add/', views.add_product, name='add_product')
+    path('add/', views.add_product, name='add_product'),
+    path('edit/<int:product_id>/', views.edit_product, name='edit_product'),
 ]
