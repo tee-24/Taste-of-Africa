dabf781c761981a2c5e3a50f25021368a1531808
Towbie Abiodun-Idowu <towbieidowu@gmail.com>
2024-12-15 11:17:49 +0000

    Complete confirmation email

--- taste_of_africa/settings.py
+++ taste_of_africa/settings.py
@@ -175,3 +175,4 @@ STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
 STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+DEFAULT_FROM_EMAIL = 'boutiqueado@example.com'
