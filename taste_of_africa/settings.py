81eb2a3e487396a39f7c62a9646b010c98ff1a7e
Towbie Abiodun-Idowu <towbieidowu@gmail.com>
2024-12-15 16:25:05 +0000

    Set debug

--- taste_of_africa/settings.py
+++ taste_of_africa/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['8000-tee24-tasteofafrica-1kyo6pfi0a7.ws.codeinstitute-ide.net', 'localhost', 'taste-of-africa1.herokuapp.com']
 
