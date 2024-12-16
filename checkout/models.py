a86aa745317640b6384204f6be04f0fb75d2c04d
Towbie Abiodun-Idowu <towbieidowu@gmail.com>
2024-12-16 21:11:46 +0000

    Add more products

--- checkout/models.py
+++ checkout/models.py
@@ -36,7 +36,7 @@ class Order(models.Model):
         """
         Generate a random, unique order number using UUID
         """
-        return uuid.uuid4().hex.upper()
+        return uuid.uuid4().hex[:7].upper()
 
     def update_total(self):
         """
